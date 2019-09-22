3ab5fdc57522b7d34660d89e6daf91c25519e293
tomaspoupin <54379361+tomaspoupin@users.noreply.github.com>
2019-09-22 04:16:38 -0300

    changed only a small mistake

--- scripts/backend/basic_structures/subroutines/schedule_algebra.py
+++ scripts/backend/basic_structures/subroutines/schedule_algebra.py
@@ -4,7 +4,7 @@ def vector_sum(vector1, vector2):   # It will be used to sum the schedule of eac
     sum_vector = []
     length = len(vector1)
     if len(vector1)==len(vector2):  # It will be 7 in both cases anyways
-        for index in range(0, length-1):
+        for index in range(length):
             sum_vector.append( vector1[index] + vector2[index] )
     return sum_vector
 
